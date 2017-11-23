f7823a47e2fe4ed88fb9d6d788268a5f3309ea7c
Michal Krok <michal.krok94@gmail.com>
2017-11-23 11:59:04 +0100

    Model for calculations - star and vector algebra.

---
+++ lab2_N-body/star.py
@@ -0,0 +1,55 @@
+import math
+from random import randint
+
+
+class Vector3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, scalar):
+        return Vector3D(scalar * self.x, scalar * self.y, scalar * self.z)
+
+    def __rmul__(self, scalar):
+        return self * scalar
+
+    def __abs__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def __eq__(self, other):
+        return abs(self - other) < 0.001
+
+
+Position = Vector3D
+Velocity = Vector3D
+Acceleration = Vector3D
+Force = Vector3D
+
+
+class Star:
+    def __init__(self, mass, position, velocity):
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        self.force = Force(0, 0, 0)
+
+    def reset(self):
+        self.force = Force(0, 0, 0)
+
+    @staticmethod
+    def _generate():
+        mass = randint(5000, 100000)    # 1e27kg
+        position = Position(randint(0, 50000), randint(0, 50000), randint(0, 1000))     # light years ~= 9.4e15m
+        velocity = Velocity(randint(0, 100), randint(0, 100), randint(0, 100))      # km/s
+        return Star(mass, position, velocity)
+
+    @staticmethod
+    def generate(n):
+        return [Star._generate() for _ in range(n)]
